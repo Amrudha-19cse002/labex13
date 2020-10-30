c9e0349ddd9c5852d23b1da4a0982a420af26de6
Amrudha-19cse002 <68490508+Amrudha-19cse002@users.noreply.github.com>
2020-10-29 23:36:48 -0700

    Add files via upload

---
+++ labex13/bookshop.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package labex13;
+
+/**
+ *
+ * @author Desktop pc
+ */
+public class boookshop
+{
+    String title;
+    String author;
+    String publisher;
+    double price;
+    int stock;
+    boook(String t,String a,String p,double c,int s)
+    {
+        title=t;
+        author=a;
+        publisher=p;
+        price=c;
+        stock=s;
+    }
+}
